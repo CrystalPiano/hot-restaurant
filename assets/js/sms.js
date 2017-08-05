1cda695c3931f7a7c317e545d807e5ccf8d1cf8b
LucFII <LSFIIChi@Gmail.com>
2017-08-05 12:10:03 -0500

    sms.js functioning

--- assets/js/sms.js
+++ assets/js/sms.js
@@ -1,12 +1,13 @@
-var client = require('twilio')(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+var accountSid = 'AC6db6a30291d12b8d6c44c4d71885f5b3'; // Your Account SID from www.twilio.com/console
+var authToken = '96b991672e75a2f878b7c998bbe45a15';   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
  
 client.messages.create({
-  from: process.env.TWILIO_PHONE_NUMBER,
-  to: process.env.CELL_PHONE_NUMBER,
-  body: "You just sent an SMS from Node.js using Twilio!"
+  from: "+12248777213",
+  to: "+13122869811",
+  body: "Your table is ready! Ba-Ba-BOP!!!"
 }, function(err, message) {
   if(err) {
     console.error(err.message);
